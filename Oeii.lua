cd390d2c43fa4c5d5d43c1280a7e06bf24c034d9
lordoverlord40-wq <lordoverlord40@gmail.com>
2026-07-25 02:43:17 -0600

    Update Oeii.lua

--- Oeii.lua
+++ Oeii.lua
@@ -2,7 +2,7 @@ local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/imhen
 
 local window = library:AddWindow("ALEKING HUB | Private Farming", {
     main_color = Color3.fromRGB(0, 0, 0),
-    title_bar = {Color3.fromRGB(220, 220, 220), Color3.fromRGB(80, 80, 80)},
+    title_bar = {Color3.fromRGB(0, 0, 0), Color3.fromRGB(0, 0, 0)},
     background = {Color3.fromRGB(0, 0, 0)},
     background_transparency = 0.3,
     min_size = Vector2.new(600, 400),
